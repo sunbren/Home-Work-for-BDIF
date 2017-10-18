88cea4a7a540659c4c3a100ef40884ab675f66df
elara15598 <sunbren@163.com>
2017-10-18 20:02:04 +0800

    TEST1

---
+++ HW3.R
@@ -0,0 +1,5 @@
+Homework3 
+Question1.
+#install.packages("digest",repos='http://cran.us.r-project.org')
+library(digest)
+digest("I learn a lot from this class when I am proper listening to the professor","sha256")
