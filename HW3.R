bfa906ee9bb27ea6d393a56f118f6d2ec98471a6
Bingren Sun026 <32771578+sunbren@users.noreply.github.com>
2017-10-18 23:20:20 +0800

    Update HW3.R

--- HW3.R
+++ HW3.R
@@ -3,6 +3,7 @@ Question1.
 #install.packages("digest",repos='http://cran.us.r-project.org')
 library(digest)
 digest("I learn a lot from this class when I am proper listening to the professor","sha256")
+digest("I do not learn a lot from this class when I am absent and playing on my Iphone","sha256")
 
 Question3
 library(readr)
